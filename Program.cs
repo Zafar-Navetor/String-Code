df3cd0c2d73c8d7116d06169128eedda6ec0ebc3
zafar <zfrahmad@gmail.com>
2019-10-13 06:24:00 -0700

    String code here

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = "Hello";
+            String b = "Testing";
+
+            String c = a + " " + b;
+
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
